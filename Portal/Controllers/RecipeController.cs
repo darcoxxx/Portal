fef841b97b80e64de43f6c439f5af5c12d1de3a5
Darek Charkot <darcoch1@gmail.com>
2013-02-28 21:22:06 +0100

    Init commit

---
+++ Portal/Controllers/RecipeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Portal.Models;
+
+namespace Portal.Controllers
+{
+    public class RecipeController : Controller
+    {
+      private readonly RecipeDbContext _recipeRepo;  
+      
+      public RecipeController()
+      {
+        _recipeRepo = new RecipeDbContext();
+      }
+
+        //
+        // GET: /Recipe/
+
+        public ActionResult Index()
+        {
+            return View(_recipeRepo.Recipes.ToList());
+        }
+
+        //
+        // GET: /Recipe/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            RecipeModels recipemodels = _recipeRepo.Recipes.Find(id);
+            if (recipemodels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipemodels);
+        }
+
+        //
+        // GET: /Recipe/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Recipe/Create
+
+        [HttpPost]
+        public ActionResult Create(RecipeModels recipemodels)
+        {
+            if (ModelState.IsValid)
+            {
+              recipemodels.DataDodania = DateTime.Now;
+              recipemodels.Autor = User.Identity.Name;
+                _recipeRepo.Recipes.Add(recipemodels);
+                _recipeRepo.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(recipemodels);
+        }
+
+        //
+        // GET: /Recipe/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            RecipeModels recipemodels = _recipeRepo.Recipes.Find(id);
+            if (recipemodels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipemodels);
+        }
+
+        //
+        // POST: /Recipe/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(RecipeModels recipemodels)
+        {
+            if (ModelState.IsValid)
+            {
+                _recipeRepo.Entry(recipemodels).State = EntityState.Modified;
+                _recipeRepo.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(recipemodels);
+        }
+
+        //
+        // GET: /Recipe/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            RecipeModels recipemodels = _recipeRepo.Recipes.Find(id);
+            if (recipemodels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipemodels);
+        }
+
+        //
+        // POST: /Recipe/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            RecipeModels recipemodels = _recipeRepo.Recipes.Find(id);
+            _recipeRepo.Recipes.Remove(recipemodels);
+            _recipeRepo.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _recipeRepo.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Portal/Models/RecipeModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Portal.Models
+{
+  public class RecipeModels
+  {
+    public int ID { get; set; }
+    [Display(Name = "Nazwa przepisu")]
+    public string NazwaPrzepis { get; set; }
+    public string Autor { get; set; }
+    public string Kategoria { get; set; }
+    [Display(Name = "Składniki")]
+    public string Skladniki { get; set; }
+    public string Wykonanie { get; set; }
+    [Display(Name = "Data dodania")]
+    [DisplayFormat(DataFormatString = "{0:d}")]
+    public DateTime? DataDodania { get; set; }
+    [Display(Name = "Data modyfikacji")]
+    public DateTime? DataModyfikacji { get; set; }
+  }
+}
